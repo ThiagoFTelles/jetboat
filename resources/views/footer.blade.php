87e028c521ffc99a3258330c5adf5942349df1b5
Thiago Telles <atendimento.telles@gmail.com>
2019-04-04 18:39:59 -0300

    adicionei o ext-bcmath e moontoast/math ao require do composer.json

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,7 +5,7 @@
 </div> -->
 
  <!-- Hero footer: will stick at the bottom -->
- <div class="hero-foot" id="appx">
+ <div class="hero-foot" id="app">
     <nav class="tabs is-boxed is-fullwidth">
       <div class="container">
         <ul>
