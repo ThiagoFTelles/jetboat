c05b9144a3bcfbbe36823c7bfc879824b8200c87
Thiago Telles <atendimento.telles@gmail.com>
2019-04-29 01:02:44 -0300

    adicionei legendas de data inicial e final no filtro de datas do relatório

--- resources/views/date-range.blade.php
+++ resources/views/date-range.blade.php
@@ -3,16 +3,12 @@
     {{ method_field('GET') }}
     <div class="field" >
         <label class="label" for="date-initial">Data Inicial</label>
-        <div class="control">
             <input class="input is-rounded  {{ $errors -> has('date-initial') ? 'is-danger' : '' }} " style="width: auto;margin: 5px 0;" value="{{ $di }}" type="date" id="date-initial" name="date-initial" onfocusout="validaDados()" data-date="" data-date-format="DD MMMM YYYY" required>
-        </div>        
     </div>
 
     <div class="field" >
         <label class="label" for="date-final">Data Final</label>
-        <div class="control">
-            <input class="input is-rounded  {{ $errors -> has('date-final') ? 'is-danger' : '' }} " style="width: auto;margin: 5px 0;" value="{{ $di }}" type="date" id="date-final" name="date-final" onfocusout="validaDados()" data-date="" data-date-format="DD MMMM YYYY" required>
-        </div>        
+            <input class="input is-rounded  {{ $errors -> has('date-final') ? 'is-danger' : '' }} " style="width: auto;margin: 5px 0;" value="{{ $di }}" type="date" id="date-final" name="date-final" onfocusout="validaDados()" data-date="" data-date-format="DD MMMM YYYY" required>    
     </div>
             <button type="submit" class="button is-primary">Aplicar</button>
     <!-- <button style="width: auto;margin: 5px 0;"  class="button is-primary" type="submit">Aplicar</button> -->
