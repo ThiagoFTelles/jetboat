913f3c39747427fdd338037f165398577eddfd5c
ThiagoFTelles <atendimento.telles@gmail.com>
2020-06-09 23:09:57 -0300

    começando a criar a API
    o get no /api/vehicles funcionando de maneira inicial

---
+++ app/Http/Controllers/Api/VehiclesController.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Vehicle;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Validation\Rule;
+use App\Datasheet;
+use App\Http\Controllers\Controller;
+
+// use GuzzleHttp\Client;
+
+class VehiclesController extends Controller
+{
+    public function __construct()
+    {
+
+        // $this->middleware('auth'); TIRAR ESTE COMENTÁRIO DEPOIS QUE DESCOBRIR COMO USAR AUTH NA API ************************************************
+    }
+
+    public function index()
+    {
+        return Vehicle::all();
+    }
+
+    public function create()
+    {
+        return view('vehicles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $atributes = $this->validateVehicle();
+
+        $atributes['marina_id'] = auth()->id();
+        $atributes['uuid'] = uuid();
+        $atributes['last_run'] = new Carbon('now', 'America/Sao_Paulo');
+        $atributes['status'] = 'parked';
+
+        $atributes['belongings'] = $request->input('belongings');
+        $atributes['gas_percentage'] = $request->input('gas_percentage');
+        $atributes['navigation_hours'] = $request->input('navigation_hours');
+
+        $vehicle = Vehicle::create($atributes);
+
+        flash('Sua embarcação foi cadastrada.');
+
+        return redirect('/vehicles');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehicle $vehicle)
+    {
+        $this->authorize('update', $vehicle);
+
+        return view('vehicles.show', compact('vehicle'));
+    }
+
+    public function showDatasheets(Vehicle $vehicle)
+    {
+        $this->authorize('update', $vehicle);
+        $datasheets = Datasheet::where('vehicle_id', $vehicle->id);
+        return view('vehicles.datasheet', compact('vehicle', 'datasheets'));
+    }
+
+    public function showParked()
+    {
+        $vehicles = auth()->user()->vehicles;
+        return view('vehicles.showParked', compact('vehicles'));
+    }
+
+    public function showNavigating()
+    {
+        $vehicles = auth()->user()->vehicles;
+        return view('vehicles.showNavigating', compact('vehicles'));
+    }
+
+    public function showOut()
+    {
+        $vehicles = auth()->user()->vehicles;
+        return view('vehicles.showOut', compact('vehicles'));
+    }
+
+    public function showDisused()
+    {
+        $vehicles = auth()->user()->vehicles;
+        return view('vehicles.showDisused', compact('vehicles'));
+    }
+
+    public function edit(Vehicle $vehicle)
+    {
+        $this->authorize('update', $vehicle);
+
+        return view('vehicles.edit', compact('vehicle'));
+    }
+
+    public function actionMenu(Vehicle $vehicle)
+    {
+        //$this->authorize('update', $vehicle); //acessado apenas pela Marina dona da embarcação, sem QR code
+        return view('vehicles.actionMenu', compact('vehicle'));
+    }
+
+    public function generateQr(Request $request, Vehicle $vehicle)
+    {
+        return view('vehicles.sticker', compact('vehicle'));
+    }
+
+    public function generatePDF(Vehicle $vehicle)
+    {
+        $name = $vehicle->owner_name;
+        $file = $name . '.pdf';
+
+        $pdf = \PDF::loadView('vehicles.pdf', compact('vehicle'))
+            ->setPaper([0, 0, 198.425, 198.425], 'landscape')->setOptions(['dpi' => 300, 'defaultFont' => 'sans-serif']); // 198.425 points = 7cm
+        return $pdf->download($file);
+    }
+
+    public function generateMiniPDF(Vehicle $vehicle)
+    {
+        $name = substr($vehicle->owner_name, 0, 10);
+        $file = $name . '.pdf';
+
+        $pdf = \PDF::loadView('vehicles.mini-pdf', compact('vehicle'))
+            ->setPaper([0, 0, 65, 49], 'landscape')->setOptions(['dpi' => 600, 'defaultFont' => 'sans-serif']); // 56.6929 points = 2cm
+        return $pdf->download($file);
+    }
+
+    public function action(Request $request, Vehicle $vehicle)
+    {
+        //$this->authorize('update', $vehicle); //acessado apenas pela Marina dona da embarcação, sem QR code
+
+        $vehicle->status = $request->input('action');
+        $vehicle->belongings = $request->input('belongings');
+        $vehicle->gas_percentage = $request->input('gas_percentage');
+        $vehicle->navigation_hours = $request->input('navigation_hours');
+        $vehicle->marina_id = auth()->user()->id;
+
+        switch ($request->get('action')) {
+            case 'deleted':
+                $vehicle->marina_id = 0;
+                break;
+            case 'run':
+                $vehicle->last_run = now();
+                $vehicle->status = 'parked';
+                break;
+            case 'navigating':
+                $vehicle->last_run = now();
+                break;
+            case 'parked':
+                $vehicle->last_run = now();
+                break;
+        }
+
+        if ($vehicle->marina_id === null) {
+            $vehicle->marina_id = auth()->user()->id;
+        }
+
+        $vehicle->update();
+        flash('Embarcação ' . $vehicle->name . ' atualizada.');
+        return redirect('/marina');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vehicle $vehicle)
+    {
+        $vehicleId = $vehicle->id;
+
+        $vehicle->belongings = $request->input('belongings');
+        $vehicle->gas_percentage = $request->input('gas_percentage');
+        $vehicle->navigation_hours = $request->input('navigation_hours');
+
+        $vehicle->update($this->reValidateVehicle($vehicleId));
+        flash('Embarcação ' . $vehicle->name . ' atualizada.');
+        return redirect('/vehicles');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vehicle $vehicle)
+    {
+        $this->authorize('update', $vehicle);
+        $vehicle->delete();
+        flash('Embarcação apagada.');
+        return redirect('/vehicles');
+    }
+
+    protected function validateVehicle()
+    {
+        return request()->validate([
+            'name' => ['required', 'min:2'],
+            'owner_name' => ['required', 'min:2'],
+            'brand' => ['min:3'],
+            'model' => ['min:3'],
+            'year' => ['min:4'],
+            'register_number' => ['required', 'min:5', 'unique:vehicles'],
+        ]);
+    }
+
+    protected function reValidateVehicle($vehicleId)
+    {
+        return request()->validate([
+            'name' => ['required', 'min:2'],
+            'owner_name' => ['required', 'min:2'],
+            'brand' => ['min:3'],
+            'model' => ['min:3'],
+            'year' => ['min:4'],
+            'register_number' => ['required', 'min:5', Rule::unique('vehicles')->ignore($vehicleId)],
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->name('api.')->group(function () {
+    Route::get('/vehicles', 'VehiclesController@index')->name('vehicles');
+});
+
+// Route::get('/ok', function () {
+//     return ['status' => true];
+// });
+
+// Route::apiResource('vehicles', 'vehiclesController');
