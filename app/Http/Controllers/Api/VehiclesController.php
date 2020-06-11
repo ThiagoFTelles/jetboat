bd18b9fe389ac9cfed6dc4f4a97287d92ddcb661
ThiagoFTelles <atendimento.telles@gmail.com>
2020-06-11 02:10:41 -0300

    Funcionando sem autenticação: GET, POST, PUT na API

--- app/Http/Controllers/Api/VehiclesController.php
+++ app/Http/Controllers/Api/VehiclesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\API\ApiError;
 use App\Vehicle;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -13,191 +14,106 @@ use App\Http\Controllers\Controller;
 
 class VehiclesController extends Controller
 {
-    public function __construct()
-    {
 
+    private $vehicle;
+
+    public function __construct(Vehicle $vehicle)
+    {
+        $this->vehicle = $vehicle;
         // $this->middleware('auth'); TIRAR ESTE COMENTÁRIO DEPOIS QUE DESCOBRIR COMO USAR AUTH NA API ************************************************
     }
 
     public function index()
     {
-        return Vehicle::all();
+        $data = ['data' => $this->vehicle->paginate(10)];
+        return response()->json($data);
     }
 
-    public function create()
+    public function show(Vehicle $uuid)
     {
-        return view('vehicles.create');
+        $data = ['data' => $uuid];
+        return response()->json($data);
     }
 
     public function store(Request $request)
     {
-        $atributes = $this->validateVehicle();
+        try {
+            $atributes = $this->validateVehicle();
 
-        $atributes['marina_id'] = auth()->id();
-        $atributes['uuid'] = uuid();
-        $atributes['last_run'] = new Carbon('now', 'America/Sao_Paulo');
-        $atributes['status'] = 'parked';
+            $atributes['marina_id'] = 1;
+            //TODO:  $atributes['marina_id'] = auth()->id();
+            $atributes['uuid'] = uuid();
+            $atributes['last_run'] = new Carbon('now', 'America/Sao_Paulo');
+            $atributes['status'] = 'parked';
+            $atributes['belongings'] = $request->input('belongings');
+            $atributes['gas_percentage'] = $request->input('gas_percentage');
+            $atributes['navigation_hours'] = $request->input('navigation_hours');
 
-        $atributes['belongings'] = $request->input('belongings');
-        $atributes['gas_percentage'] = $request->input('gas_percentage');
-        $atributes['navigation_hours'] = $request->input('navigation_hours');
+            $vehicle = Vehicle::create($atributes);
 
-        $vehicle = Vehicle::create($atributes);
-
-        flash('Sua embarcação foi cadastrada.');
-
-        return redirect('/vehicles');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Vehicle  $vehicle
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Vehicle $uuid)
-    {
-        return $uuid;
-    }
-
-    public function showDatasheets(Vehicle $vehicle)
-    {
-        $this->authorize('update', $vehicle);
-        $datasheets = Datasheet::where('vehicle_id', $vehicle->id);
-        return view('vehicles.datasheet', compact('vehicle', 'datasheets'));
-    }
-
-    public function showParked()
-    {
-        $vehicles = auth()->user()->vehicles;
-        return view('vehicles.showParked', compact('vehicles'));
-    }
-
-    public function showNavigating()
-    {
-        $vehicles = auth()->user()->vehicles;
-        return view('vehicles.showNavigating', compact('vehicles'));
-    }
-
-    public function showOut()
-    {
-        $vehicles = auth()->user()->vehicles;
-        return view('vehicles.showOut', compact('vehicles'));
-    }
-
-    public function showDisused()
-    {
-        $vehicles = auth()->user()->vehicles;
-        return view('vehicles.showDisused', compact('vehicles'));
-    }
-
-    public function edit(Vehicle $vehicle)
-    {
-        $this->authorize('update', $vehicle);
-
-        return view('vehicles.edit', compact('vehicle'));
-    }
-
-    public function actionMenu(Vehicle $vehicle)
-    {
-        //$this->authorize('update', $vehicle); //acessado apenas pela Marina dona da embarcação, sem QR code
-        return view('vehicles.actionMenu', compact('vehicle'));
-    }
-
-    public function generateQr(Request $request, Vehicle $vehicle)
-    {
-        return view('vehicles.sticker', compact('vehicle'));
-    }
-
-    public function generatePDF(Vehicle $vehicle)
-    {
-        $name = $vehicle->owner_name;
-        $file = $name . '.pdf';
-
-        $pdf = \PDF::loadView('vehicles.pdf', compact('vehicle'))
-            ->setPaper([0, 0, 198.425, 198.425], 'landscape')->setOptions(['dpi' => 300, 'defaultFont' => 'sans-serif']); // 198.425 points = 7cm
-        return $pdf->download($file);
-    }
-
-    public function generateMiniPDF(Vehicle $vehicle)
-    {
-        $name = substr($vehicle->owner_name, 0, 10);
-        $file = $name . '.pdf';
-
-        $pdf = \PDF::loadView('vehicles.mini-pdf', compact('vehicle'))
-            ->setPaper([0, 0, 65, 49], 'landscape')->setOptions(['dpi' => 600, 'defaultFont' => 'sans-serif']); // 56.6929 points = 2cm
-        return $pdf->download($file);
-    }
-
-    public function action(Request $request, Vehicle $vehicle)
-    {
-        //$this->authorize('update', $vehicle); //acessado apenas pela Marina dona da embarcação, sem QR code
-
-        $vehicle->status = $request->input('action');
-        $vehicle->belongings = $request->input('belongings');
-        $vehicle->gas_percentage = $request->input('gas_percentage');
-        $vehicle->navigation_hours = $request->input('navigation_hours');
-        $vehicle->marina_id = auth()->user()->id;
-
-        switch ($request->get('action')) {
-            case 'deleted':
-                $vehicle->marina_id = 0;
-                break;
-            case 'run':
-                $vehicle->last_run = now();
-                $vehicle->status = 'parked';
-                break;
-            case 'navigating':
-                $vehicle->last_run = now();
-                break;
-            case 'parked':
-                $vehicle->last_run = now();
-                break;
+            $return = ['data' => ['msg' => 'Veículo criado com sucesso!', 'vehicle' => $vehicle]];
+            return response()->json($return, 201);
+        } catch (\Exception $error) {
+            if (config('app.debug')) {
+                return response()->json(
+                    ApiError::errorMessage($error->getMessage(), 1010)
+                    //TODO: Fazer lista de códigos de erros, aqui eu considero que este erro tem o código fictício = 1010.
+                );
+            }
+            //aqui em baixo, em vez de usar "$error->getMessage()" eu posso criar uma mensagem padrão para aparecer quando o debug estiver desativado.
+            return response()->json(ApiError::errorMessage($error->getMessage(), 1010));
         }
+    }
 
-        if ($vehicle->marina_id === null) {
-            $vehicle->marina_id = auth()->user()->id;
+    public function update(Request $request, $uuid)
+    {
+        try {
+            $vehicle = $this->vehicle->find($uuid);
+            $vehicleId = $vehicle->id;
+            $vehicle->belongings = $request->input('belongings');
+            $vehicle->gas_percentage = $request->input('gas_percentage');
+            $vehicle->navigation_hours = $request->input('navigation_hours');
+
+            $vehicle->update($this->reValidateVehicle($vehicleId));
+
+            $return = ['data' => ['msg' => 'Veículo atualizado com sucesso!', 'vehicle' => $vehicle]];
+            return response()->json($return, 201);
+        } catch (\Exception $error) {
+            if (config('app.debug')) {
+                return response()->json(
+                    ApiError::errorMessage($error->getMessage(), 1011)
+                    //TODO: Fazer lista de códigos de erros, aqui eu considero que este erro tem o código fictício = 1011.
+                );
+            }
+            //aqui em baixo, em vez de usar "$error->getMessage()" eu posso criar uma mensagem padrão para aparecer quando o debug estiver desativado.
+            return response()->json(ApiError::errorMessage($error->getMessage(), 1011));
         }
-
-        $vehicle->update();
-        flash('Embarcação ' . $vehicle->name . ' atualizada.');
-        return redirect('/marina');
     }
 
+    // public function update(Request $request, Vehicle $vehicle)
+    // {
+    //     try {
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Vehicle  $vehicle
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Vehicle $vehicle)
-    {
-        $vehicleId = $vehicle->id;
+    //         $vehicleId = $vehicle->id;
+    //         $vehicle->belongings = $request->input('belongings');
+    //         $vehicle->gas_percentage = $request->input('gas_percentage');
+    //         $vehicle->navigation_hours = $request->input('navigation_hours');
 
-        $vehicle->belongings = $request->input('belongings');
-        $vehicle->gas_percentage = $request->input('gas_percentage');
-        $vehicle->navigation_hours = $request->input('navigation_hours');
+    //         $vehicle->update($this->reValidateVehicle($vehicleId));
+    //         $return = ['data' => ['msg' => 'Veículo criado com sucesso!', 'vehicle' => $vehicle]];
+    //         return response()->json($return, 201);
+    //     } catch (\Exception $error) {
+    //         if (config('app.debug')) {
+    //             return response()->json(
+    //                 ApiError::errorMessage($error->getMessage(), 1011)
+    //                 //TODO: Fazer lista de códigos de erros, aqui eu considero que este erro tem o código fictício = 1011.
+    //             );
+    //         }
+    //         //aqui em baixo, em vez de usar "$error->getMessage()" eu posso criar uma mensagem padrão para aparecer quando o debug estiver desativado.
+    //         return response()->json(ApiError::errorMessage($error->getMessage(), 1011));
+    //     }
 
-        $vehicle->update($this->reValidateVehicle($vehicleId));
-        flash('Embarcação ' . $vehicle->name . ' atualizada.');
-        return redirect('/vehicles');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Vehicle  $vehicle
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Vehicle $vehicle)
-    {
-        $this->authorize('update', $vehicle);
-        $vehicle->delete();
-        flash('Embarcação apagada.');
-        return redirect('/vehicles');
-    }
+    // }
 
     protected function validateVehicle()
     {
@@ -214,12 +130,12 @@ class VehiclesController extends Controller
     protected function reValidateVehicle($vehicleId)
     {
         return request()->validate([
-            'name' => ['required', 'min:2'],
-            'owner_name' => ['required', 'min:2'],
+            'name' => ['min:2'],
+            'owner_name' => ['min:2'],
             'brand' => ['min:3'],
             'model' => ['min:3'],
             'year' => ['min:4'],
-            'register_number' => ['required', 'min:5', Rule::unique('vehicles')->ignore($vehicleId)],
+            'register_number' => ['min:5', Rule::unique('vehicles')->ignore($vehicleId)],
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::namespace('Api')->name('api.')->group(function () {
     Route::prefix('/vehicles')->group(function () {
-        Route::get('/', 'VehiclesController@index')->name('vehicles');
-        Route::get('/{uuid}', 'VehiclesController@show')->name('vehicle');
+        Route::get('/', 'VehiclesController@index')->name('index_vehicles');
+        Route::get('/{uuid}', 'VehiclesController@show')->name('single_vehicle');
+        Route::post('/', 'VehiclesController@store')->name('store_vehicle');
+        Route::put('/{uuid}', 'VehiclesController@update')->name('update_vehicle');
     });
 });
 
